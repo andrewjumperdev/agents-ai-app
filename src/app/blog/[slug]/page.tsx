2bef78e2ccfcb1cf6b9d28a205a5d8c65e0b9162
andrewjumperdev <andrew.jumper.ent@gmail.com>
2025-08-13 15:35:24 +0200

    feat:Bug Navbar Variable

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -7,8 +7,8 @@ interface Params {
   };
 }
 
-export default async function PostPage({ params }: Params) {
-  const post = await getPostBySlug(params.slug);
+export default function PostPage({ params }: Params) {
+  const post = getPostBySlug(params.slug);
 
   return (
     <main className="max-w-3xl mx-auto px-6 py-12">
