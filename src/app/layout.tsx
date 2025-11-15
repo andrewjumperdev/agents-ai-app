deaf2fc645c571883719336492ae0801aa1f488c
andrewjumperdev <andrew.jumper.ent@gmail.com>
2025-11-15 01:54:52 +0100

    bug:Meta Ads-Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,7 +116,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '811488458545827');
+            fbq('init', '1517707092781568');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -126,7 +126,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=811488458545827&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1517707092781568&ev=PageView&noscript=1"
           />
         </noscript>
 
